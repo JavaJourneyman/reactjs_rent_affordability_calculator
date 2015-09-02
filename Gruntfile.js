76d2418ea9557e79ab41ee921b2b64de09d7561c
peter vijeh <peter@peters-MacBook-Pro.local>
2015-09-01 21:06:59 -0400

    adding grunt command to upload to cloudfiles

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,10 +22,30 @@ module.exports = function(grunt) {
         },
         watch: {
 
+        },
+        cloudfiles: {
+          prod: {
+            // need to input cloudfiles data 
+            'user': 'your Rackspace username',
+            'key': 'your Rackspace API key',
+            'region': 'DFW',
+            'upload': [{
+              'container': 'name of your Cloud Files container',
+              'src': 'source/static/**/*',
+              'dest': 'some/folder/',
+              'stripcomponents': 1,
+              'purge': {
+                'emails': ['your@email.com'],
+                'files': ['index.html']
+              }
+            }]
+          }
         }
     });
 
     require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+     grunt.loadNpmTasks('grunt-cloudfiles'); 
 
     grunt.registerTask('default', ['connect', 'open', 'watch']);    
 };
