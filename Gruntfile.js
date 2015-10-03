e6e169300d1e70c0cc86cf828c9e22a2e7440ac8
peter vijeh <peter@peters-MacBook-Pro.local>
2015-10-02 20:08:34 -0400

    stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,41 +9,88 @@ module.exports = function(grunt) {
     };
 
     grunt.initConfig({
-        options: options,
-        pkg: grunt.file.readJSON('package.json'),
-        connect: {
-            server: {
-                options: {
-                    port: options.port,
-                    base: '.'
+          options: options,
+          pkg: grunt.file.readJSON('package.json'),
+          connect: {
+              server: {
+                  options: {
+                      port: options.port,
+                      base: '.'
+                  }
+              }
+          },
+          open: {
+              server: {
+                  path: 'http://localhost:<%= options.port %>/'
+              }
+          },
+          react: {
+              combined_file_output: {
+                files: {
+                  'js/combined.js': [
+                    'js/actions.js',
+                    'js/store.js',
+                    'js/components.jsx.js'
+                  ]
                 }
+              }
+            },
+          uglify: {
+              options: {
+                mangle: false
+              },
+              my_target: {
+                files: {  
+                  'build/app.js': [
+                  'app/bower_components/jquery/dist/jquery.min.js',
+                  'app/bower_components/react/react-with-addons.js',
+                  'app/bower_components/lodash/lodash.js',
+                  'app/bower_components/reflux/dist/reflux.js',
+                  'app/bower_components/numeral/numeral.js',
+                  'js/combined.js'
+                  ] 
+                }
+              }
+          },
+          cssmin: {
+            // need to set folder paths 
+            target: {
+              files: [{
+                expand: true,
+                cwd: 'css/',
+                src: ['*.css', '!*.min.css'],
+                dest: 'build/',
+                ext: '.min.css'
+              }]
             }
-        },
-        open: {
-            server: {
-                path: 'http://localhost:<%= options.port %>/'
-            }
-        },
-        watch: {
+          },
+          exec: {
+            copy_assets: 'cp -R assets build/'
+          },
+          watch: {
 
-        },
-        cloudfiles: {
-          prod: {
-            'user': config.cloudfiles.user,
-            'key': config.cloudfiles.key,
-            'region': config.cloudfiles.region,
-            'upload': [{
-              'container': config.cloudfiles.container,
-              'src': config.cloudfiles.source, 
-              'dest': config.cloudfiles.destination
-            }]
+          },
+          cloudfiles: {
+            prod: {
+              'user': config.cloudfiles.user,
+              'key': config.cloudfiles.key,
+              'region': config.cloudfiles.region,
+              'upload': [{
+                'container': config.cloudfiles.container,
+                'src': config.cloudfiles.source, 
+                'dest': config.cloudfiles.destination
+              }]
+            }
           }
-        }
     });
 
     require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
      grunt.loadNpmTasks('grunt-cloudfiles'); 
-
-    grunt.registerTask('default', ['connect', 'open', 'watch']);    
+     grunt.loadNpmTasks('grunt-contrib-uglify');
+     grunt.loadNpmTasks('grunt-contrib-cssmin');
+     grunt.loadNpmTasks('grunt-react');
+     grunt.loadNpmTasks('grunt-exec');
+      grunt.registerTask('default', ['connect', 'open', 'watch']);    
+      grunt.registerTask('build', ['react', 'uglify', 'cssmin']);    
 };
