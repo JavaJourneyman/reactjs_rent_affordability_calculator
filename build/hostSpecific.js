4f0baae420e20000bd06ca948a69284a6ee65660
peter vijeh <peter@peters-MacBook-Pro.local>
2015-11-02 21:37:10 -0500

    fixing url

--- build/hostSpecific.js
+++ build/hostSpecific.js
@@ -1,10 +1,10 @@
 var pasteContent = ''; 
 var randomLinkText = [
-'The <a href="http://www.movingtonewyorkguide.com/how-much-rent-can-you-afford-calculator/">How Much Rent Can I Afford?</a> calculator was made by the folks at <a href="http://www.movingtonewyorkguide.com">Moving to New York Guide</a>', 
-'This <a href="http://www.movingtonewyorkguide.com/how-much-rent-can-you-afford-calculator/">apartment rent calculator</a> was made by the folks at <a href="http://www.movingtonewyorkguide.com">Moving to New York Guide</a>', 
-'This <a href="http://www.movingtonewyorkguide.com/how-much-rent-can-you-afford-calculator/">rent budget calculator</a> was made by the folks at <a href="http://www.movingtonewyorkguide.com">Moving to New York Guide</a>',
-'This <a href="http://www.movingtonewyorkguide.com/how-much-rent-can-you-afford-calculator/">rent affordability calculator</a> was made by the folks at <a href="http://www.movingtonewyorkguide.com">Moving to New York Guide</a>',
-'The "<a href="http://www.movingtonewyorkguide.com/how-much-rent-can-you-afford-calculator/">how much apartment can i afford?</a>" calculator was made by the folks at <a href="http://www.movingtonewyorkguide.com">Moving to New York Guide</a>',
+'The <a href="http://www.movingtonewyorkguide.com/tools/how-much-rent-calculator/">How Much Rent Can I Afford?</a> calculator was made by the folks at <a href="http://www.movingtonewyorkguide.com">Moving to New York Guide</a>', 
+'This <a href="http://www.movingtonewyorkguide.com/tools/how-much-rent-calculator/">apartment rent calculator</a> was made by the folks at <a href="http://www.movingtonewyorkguide.com">Moving to New York Guide</a>', 
+'This <a href="http://www.movingtonewyorkguide.com/tools/how-much-rent-calculator/">rent budget calculator</a> was made by the folks at <a href="http://www.movingtonewyorkguide.com">Moving to New York Guide</a>',
+'This <a href="http://www.movingtonewyorkguide.com/tools/how-much-rent-calculator/">rent affordability calculator</a> was made by the folks at <a href="http://www.movingtonewyorkguide.com">Moving to New York Guide</a>',
+'The "<a href="http://www.movingtonewyorkguide.com/tools/how-much-rent-calculator/">how much apartment can i afford?</a>" calculator was made by the folks at <a href="http://www.movingtonewyorkguide.com">Moving to New York Guide</a>',
 ];
 var rand = randomLinkText[Math.floor(Math.random() * randomLinkText.length)];
 // insert random text link at bottom of html 
